62323505ae111991ef32715e1628e5f7a30426c4
Nikolay Tupitsyn <nikolaideveloper@gmail.com>
2022-03-13 20:18:09 +0000

    Create simple CRUD. Create model and add some data. Test CRUD by postman

--- server.js
+++ server.js
@@ -15,6 +15,9 @@ const bootcampsRoutes = require('./routes/bootcamps')
 
 const app = express()
 
+// Body parser
+app.use(express.json())
+
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 }
